2c32509c1a565d72e1a75198fc3da6699dd9a5b9
Dao Hiep <hieptrang13042002@gmail.com>
2021-11-07 12:12:34 +0700

    added Bomber moving state for sprite selection

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -1,13 +1,8 @@
 package uet.oop.bomberman.entities;
 
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
 import uet.oop.bomberman.graphics.Sprite;
 
-import java.util.Random;
 
 import static uet.oop.bomberman.utils.TerminalColor.*;
 
@@ -15,6 +10,7 @@ public class Bomber extends Entity implements Moveable, Animatable {
     public boolean up, left, down, right;
     public int anime = 0;
     public boolean moving;
+    public String facingDirection = "";
 
     public Bomber(int x, int y, Image img) {
         super( x, y, img);
@@ -22,14 +18,14 @@ public class Bomber extends Entity implements Moveable, Animatable {
 
     @Override
     public void update() {
+        moveHandler();
         animate();
         imageHandler();
-        moveHandler();
     }
 
     @Override
     public void animate() {
-        if(anime < 7500) anime++;
+        if(anime < 60) anime++;
         else anime = 0;
     }
 
@@ -37,10 +33,10 @@ public class Bomber extends Entity implements Moveable, Animatable {
     @Override
     public void moveHandler() {
         int xS = 0, yS = 0;
-        if (up) yS--;
-        if (down) yS++;
-        if (left) xS--;
-        if (right) xS++;
+        if (up) yS -= 2;
+        if (down) yS += 2;
+        if (left) xS -= 2;
+        if (right) xS += 2;
 
         if (xS == 0 && yS == 0) {
             moving = false;
@@ -52,6 +48,12 @@ public class Bomber extends Entity implements Moveable, Animatable {
 
     @Override
     public void move(double xS, double yS) {
+        //Thứ tự 4 cái này là quan trọng khi nhiều nút đươc bấm cùng lúc
+        if (xS >0) facingDirection = "RIGHT";
+        if (xS <0) facingDirection = "LEFT";
+        if (yS >0) facingDirection = "DOWN";
+        if (yS <0) facingDirection = "UP";
+
         this.x += xS;
         this.y += yS;
     }
@@ -64,26 +66,30 @@ public class Bomber extends Entity implements Moveable, Animatable {
 
     @Override
     public void imageHandler() {
-        if(up) {
-            this.img = Sprite.player_up.getFxImage();
-            if (moving) {
-                this.img = Sprite.movingSprite(Sprite.player_up, Sprite.player_up_1, Sprite.player_up_2, anime, 20).getFxImage();
-            }
-        } else if (right) {
-            this.img = Sprite.player_right.getFxImage();
-            if (moving) {
-                this.img = Sprite.movingSprite(Sprite.player_right, Sprite.player_right_1, Sprite.player_right_2, anime, 20).getFxImage();
-            }
-        } else if (down) {
-            this.img = Sprite.player_down.getFxImage();
-            if (moving) {
-                this.img = Sprite.movingSprite(Sprite.player_down, Sprite.player_down_1, Sprite.player_down_2, anime, 20).getFxImage();
-            }
-        } else if (left) {
-            this.img = Sprite.player_left.getFxImage();
-            if (moving) {
-                this.img = Sprite.movingSprite(Sprite.player_left, Sprite.player_left_1, Sprite.player_left_2, anime, 20).getFxImage();
-            }
+        switch (facingDirection) {
+            case "UP":
+                this.img = Sprite.player_up.getFxImage();
+                if (moving) {
+                    this.img = Sprite.movingSprite(Sprite.player_up, Sprite.player_up_1, Sprite.player_up_2, anime, 20).getFxImage();
+                }
+                break;
+            case "DOWN":
+                this.img = Sprite.player_down.getFxImage();
+                if (moving) {
+                    this.img = Sprite.movingSprite(Sprite.player_down, Sprite.player_down_1, Sprite.player_down_2, anime, 20).getFxImage();
+                }
+                break;
+            case "LEFT":
+                this.img = Sprite.player_left.getFxImage();
+                if (moving) {
+                    this.img = Sprite.movingSprite(Sprite.player_left, Sprite.player_left_1, Sprite.player_left_2, anime, 20).getFxImage();
+                }
+                break;
+            default:
+                this.img = Sprite.player_right.getFxImage();
+                if (moving) {
+                    this.img = Sprite.movingSprite(Sprite.player_right, Sprite.player_right_1, Sprite.player_right_2, anime, 20).getFxImage();
+                }
         }
     }
 }
